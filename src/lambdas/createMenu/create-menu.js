efb255aee99b64880c7c562ddf01f4c59da3d3a1
Bakytbek Tatibekov <bakytbek.tatibekov@gmail.com>
2023-02-10 09:22:23 -0800

    FOODRATES: mock data added, func moved to folder

---
+++ src/lambdas/createMenu/create-menu.js
@@ -0,0 +1,37 @@
+const db = require("../../../db");
+const { PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall } = require("@aws-sdk/util-dynamodb");
+
+const createMenu = async (event) => {
+  const response = { statusCode: 200 };
+
+  try {
+    // const body = event.body; // in local development, parsing gives an error
+    const body = JSON.parse(event.body);
+    console.log({ body });
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Item: marshall(body || {}),
+    };
+    const createResult = await db.send(new PutItemCommand(params));
+
+    response.body = JSON.stringify({
+      message: "Successfully created post.",
+      createResult,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to create post.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+
+  return response;
+};
+
+module.exports = {
+  createMenu,
+};
