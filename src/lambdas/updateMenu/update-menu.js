170ba098cd9ffe1e4fe27bfd457cd420c04fca1c
Bakytbek Tatibekov <bakytbek.tatibekov@gmail.com>
2023-03-18 23:04:51 -0700

    checking event type

--- src/lambdas/updateMenu/update-menu.js
+++ src/lambdas/updateMenu/update-menu.js
@@ -57,10 +57,10 @@ const processPutItemRequest = (params) => {
 
 const handler = async (event) => {
   const response = { statusCode: 200 };
+  console.log("what is it? ", typeof event, event);
   try {
     const eventObject = JSON.parse(event);
     const body = JSON.parse(event.body);
-    console.log("what is it? ", typeof eventObject, eventObject);
     // const eventObject = event; // for local invovation
     // const body = event.body; // for local invovation
 
